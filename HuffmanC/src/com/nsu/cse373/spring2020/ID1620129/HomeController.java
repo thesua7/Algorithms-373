28dda6aac15f28e868cdbef80b7eb63cbfbc20d2
Sani Ahamed <thesua711@gmail.com>
2020-03-20 17:09:58 +0600

    File Selection Module Added

--- HuffmanC/src/com/nsu/cse373/spring2020/ID1620129/HomeController.java
+++ HuffmanC/src/com/nsu/cse373/spring2020/ID1620129/HomeController.java
@@ -3,34 +3,39 @@ package com.nsu.cse373.spring2020.ID1620129;
 
 import javafx.fxml.*;
 
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+
+import javafx.stage.FileChooser;
 
 
 public class HomeController {
 
-    @FXML
-    private TextField nameText;
 
-    @FXML
-    private PasswordField pText;
 
     @FXML
     private Label toShow;
+    
+   
 
-    @FXML
-    public void login(ActionEvent event) {
-
-    	if(nameText.getText().equals("sani")&&pText.getText().equals("123")) {
-    		toShow.setText("LoggedIn");
-    	}
-    	
-    	else {
-    		toShow.setText("Invalid U&P");
-    	}
-    	
-    }
+  @FXML
+   public void uploadbtn(ActionEvent event) {
+	  
+	  FileChooser fc = new FileChooser();
+	  File selectedFile = fc.showOpenDialog(null);
+	  
+	  if(selectedFile !=null) {
+		  toShow.setText(selectedFile.getName());
+	  }
+	  else {
+		  toShow.setText("Not Selected");
+	  }
+	  
+	  
+	  
+  }
+  
 	
 }
